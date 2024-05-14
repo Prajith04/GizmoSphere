eaa5611b6bbbff8300d47032354658f82a6fe9f9
Prajith04 <111488341+Prajith04@users.noreply.github.com>
2024-05-14 13:52:16 +0530

    Update settings.py

--- GizmoSphere/settings.py
+++ GizmoSphere/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-0jkg2y3hx13sxp0pee(c3807puy8=3l@njy&l--tnl+)wf&5zs"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
